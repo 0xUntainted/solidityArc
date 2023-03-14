6bbd82d7736dc0bc6c547518a86b3b4f10fbc909
Untainted <122657429+0xUntainted@users.noreply.github.com>
2023-03-14 19:01:32 +0800

    Update myBoolean.sol

--- myBoolean.sol
+++ myBoolean.sol
@@ -1,8 +1,11 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.14;
-
 /* exploring bolean in solidity, default value of bool after you declare it is false, should always have true or false while 1 and 0 doesn't work
 because of integer intervention*/
+
+/* remember that in Solidity there is no undefined or null. If you initialize a variable with out value it will automatically set to its default value. 
+0 in integer, false in boolean and "" in string.*/
+
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
 
 contract myBoolean {
     bool public myBool;

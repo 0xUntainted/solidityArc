8e1878dc3e84466aa863556d92199d37ef3cf375
Untainted <122657429+0xUntainted@users.noreply.github.com>
2023-03-14 18:57:09 +0800

    Update myBoolean.sol

--- myBoolean.sol
+++ myBoolean.sol
@@ -1,6 +1,7 @@
 /* exploring bolean in solidity, default value of bool after you declare it is false, should always have true or false while 1 and 0 doesn't work
-because of integer intervention.
-- remember, all values in Solidity are initialized by with their default value. There is no undefined or null. 
+because of integer intervention.*/
+
+/* remember, all values in Solidity are initialized by with their default value. There is no undefined or null. 
 In addtion to this unit has a default value of 0 and bool to false*/
 
 //SPDX-License-Identifier: MIT

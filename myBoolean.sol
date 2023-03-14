4dd8de189aa244cf6a0ec1817a0b4d7c3362ea0c
Untainted <122657429+0xUntainted@users.noreply.github.com>
2023-03-14 17:08:15 +0800

    Update myBoolean.sol

--- myBoolean.sol
+++ myBoolean.sol
@@ -1,8 +1,9 @@
+/* exploring bolean in solidity, default value of bool after you declare it is false, should always have true or false while 1 and 0 doesn't work
+because of integer intervention.
+- remember, all values in Solidity are initialized by with their default value. There is no undefined or null.*/
+
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.14;
-
-/* exploring bolean in solidity, default value of bool after you declare it is false, should always have true or false while 1 and 0 doesn't work
-because of integer intervention*/
 
 contract myBoolean {
     bool public myBool;

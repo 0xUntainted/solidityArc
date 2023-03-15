b50314aa835a3f804c669a2c05f64ae1ab7b0d5d
Untainted <122657429+0xUntainted@users.noreply.github.com>
2023-03-15 19:15:44 +0800

    Update myString.sol

--- myString.sol
+++ myString.sol
@@ -1,3 +1,13 @@
+/* byte as the counterpart representation of char in C++, additionally one character is == one byte.
+the keyword used for byte is 'bytes'. */
+
+/* strings cannot be converted to 'bytes' as the characters in string is made of UTF-8 which has 
+different byte representation */
+
+//strings cost expensive to store in the blockchain that's why it is not advisable to do so.
+
+// the use of keccak256 or sha256 to compare strings.
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.14;
 
